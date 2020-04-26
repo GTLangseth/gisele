bda652a44b292d5e80aa6803c7ca5424ff37591a
GTLangseth <grant.langseth@gmail.com>
2020-04-26 18:16:33 -0400

    Add code to stop/start launched instances

--- terraform/server.tf
+++ terraform/server.tf
@@ -31,7 +31,9 @@ resource "aws_security_group" "server-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 output "server_public_ip" {
   value = "${aws_instance.server.public_ip}"
+}
+output "server_id" {
+  value = "${aws_instance.server.id}"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,15 +12,3 @@ variable "amis" {
     "us-east-2" = "ami-0a80c1bfc488c6673"
   }
 }
-# variable "availability_zones" {
-#   type = "map"
-#   default = {
-#     "us-east-2" = [
-#         "us-east-2a",
-#         "us-east-2b",
-#         "us-east-2c"
-#     ]
-#   }
-# }
-# variable "vpc_id" {}
-# variable "server_subnet_id" {}
